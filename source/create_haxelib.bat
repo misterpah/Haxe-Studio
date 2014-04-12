e81431ad2c683cb043e654a6758dc9f73fb6c499
as3boyan <denis.flash.as3@gmail.com>
2014-04-12 10:40:19 +0300

    Fix for create_haxelib.bat batch script

--- source/create_haxelib.bat
+++ source/create_haxelib.bat
@@ -1,7 +1,7 @@
 del Haxe-Studio.zip
 cd ../bin
-7z a -r ../bat/Haxe-Studio.zip
-cd ../bat
+7z a -r ../source/Haxe-Studio.zip
+cd ../source
 haxelib submit Haxe-Studio.zip
 del Haxe-Studio.zip
 pause
