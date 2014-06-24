1081aadc598075e588a8e3f7a12cbff43eae3493
misterpah <misterpah@gmail.com>
2014-06-24 20:27:44 +0800

    projectTree now can change it's folder.

--- plugin/plugin.misterpah.ProjectTree/bin/projectTree.js
+++ plugin/plugin.misterpah.ProjectTree/bin/projectTree.js
@@ -1,74 +1,122 @@
-$("#tree_position").html("");
+(function(){
+	$("#tree_position").html("");
+	plugin.misterpah.ProjectTree.projectFolder = "";
 
-var folder_content = Utils.readDir(Main.session.project_folder);
+    plugin.misterpah.ProjectTree.contextMenu_file = ['<div style="position:absolute;" id="test">',
+    '<ul class="dropdown-menu" role="menu">',
+    '<li><a tabindex="-1" href="#">Set this as Project File</a></li>',
+    '</ul>',
+    '</div>'].join("\n");
 
-function openMe(obj)
-	{
-	var path = $(obj).attr('data-path');
-	//console.dir(path);
-	Main.message.broadcast("plugin.misterpah.FileAccess:OpenFileDirectly","plugin.misterpah.ProjectTree",path);
-	}
 
-function openFolder(obj)
-	{
-	var html = $(obj).html();
-	var userwantsto = "";
-	if (html[0] == "+")	 // user want to open folder
-		{
-		html = html.replace("+","-");	
-		userwantsto = "open";
-		}
-	else if (html[0] == "-")	 // user want to close folder
-		{
-		html = html.replace("-","+");	
-		userwantsto = "close";
-		
-		}
-	$(obj).html(html);
 	
-	var path = $(obj).attr('data-path');
-	
-	if (userwantsto == "close")
+    plugin.misterpah.ProjectTree.contextMenu_folder = ['<div id="plugin-misterpah-ProjectTree-contextMenu-folder">',
+    '<ul class="dropdown-menu" role="menu">',
+    '<li><a tabindex="-1" href="#">Enter this folder</a></li>',
+    '</ul>',
+    '</div>'].join("\n");
+
+	$("#temp").append(plugin.misterpah.ProjectTree.contextMenu_file);
+	$("#temp").append(plugin.misterpah.ProjectTree.contextMenu_folder);
+
+
+
+
+
+	// open file if it's clicked
+	plugin.misterpah.ProjectTree.openMe = function (obj)
 		{
-		$("ul[data-path='"+path+"']").html("");
+		var path = $(obj).attr('data-path');
+		Main.message.broadcast("plugin.misterpah.FileAccess:OpenFileDirectly","plugin.misterpah.ProjectTree",path);
 		}
 
-	if (userwantsto == "open")
+	// open/close folder & show it's content once clicked
+	plugin.misterpah.ProjectTree.openFolder = function (obj)
 		{
-		//console.dir(path);
-		var folder_content = Utils.readDir(path);
-	
-	
-		for (i = 0;i< folder_content.length;i++)
+		var html = $(obj).html();
+		var userwantsto = "";
+		if (html[0] == "+")	 // user want to open folder
 			{
-			var isfile = Utils.isFile(path + Utils.path.sep + folder_content[i]);
-			if(isfile)
+			html = html.replace("+","-");	
+			userwantsto = "open";
+			}
+		else if (html[0] == "-")	 // user want to close folder
+			{
+			html = html.replace("-","+");	
+			userwantsto = "close";
+		
+			}
+		$(obj).html(html);
+	
+		var path = $(obj).attr('data-path');
+	
+		if (userwantsto == "close")
+			{
+			$("ul[data-path='"+path+"']").html("");
+			}
+
+		if (userwantsto == "open")
+			{
+			var folder_content = Utils.readDir(path);
+	
+			for (i = 0;i< folder_content.length;i++)
 				{
-				var _temp= folder_content[i];
-				var _ext = _temp.split(".").pop();
-				$("ul[data-path='"+path+"'").append("<li><a class='file "+_ext+"' onclick='openMe($(this));'   data-path='"+path + Utils.path.sep + folder_content[i]+"' href='#'><span class='glyphicon glyphicon-file'></span> &nbsp;"+folder_content[i] +"</a></li>");
-				}
-			else
-				{
-				$("ul[data-path='"+path+"'").append("<li><a class='folder' onclick='openFolder($(this));'   data-path='"+path + Utils.path.sep + folder_content[i]+"' href='#'>+ <span class='glyphicon glyphicon-folder-open'></span> &nbsp;"+folder_content[i] +"</a><ul data-path='"+path + Utils.path.sep + folder_content[i]+"'></ul></li>");
+				var isfile = Utils.isFile(path + Utils.path.sep + folder_content[i]);
+				if(isfile)
+					{
+					var _temp= folder_content[i];
+					var _ext = _temp.split(".").pop();
+					$("ul[data-path='"+path+"'").append("<li><a class='file "+_ext+"' data-toggle='context' data-target='#plugin-misterpah-ProjectTree-contextMenu-file' onclick='plugin.misterpah.ProjectTree.openMe($(this));'   data-path='"+path + Utils.path.sep + folder_content[i]+"' href='#'><span class='glyphicon glyphicon-file'></span> &nbsp;"+folder_content[i] +"</a></li>");
+					}
+				else
+					{
+					$("ul[data-path='"+path+"'").append("<li><a class='folder' data-toggle='context' data-target='#plugin-misterpah-ProjectTree-contextMenu-folder' onclick='plugin.misterpah.ProjectTree.openFolder($(this));'   data-path='"+path + Utils.path.sep + folder_content[i]+"' href='#'>+ <span class='glyphicon glyphicon-folder-open'></span> &nbsp;"+folder_content[i] +"</a><ul data-path='"+path + Utils.path.sep + folder_content[i]+"'></ul></li>");
+					}
 				}
 			}
 		}
-	//Main.message.broadcast("plugin.misterpah.FileAccess:OpenFileDirectly","plugin.misterpah.ProjectTree",path);
-	}
 
-$("#tree_position").html("<br/><ul id='file_tree'></ul>");
-for (i = 0;i< folder_content.length;i++)
-	{
-	var isfile = Utils.isFile(Main.session.project_folder + Utils.path.sep + folder_content[i]);
-	if (isfile)
+
+
+
+	plugin.misterpah.ProjectTree.show_project_tree = function (project_folder)
 		{
-		var _temp= folder_content[i];
-		var _ext = _temp.split(".").pop();		
-		$("#file_tree").append("<li><a class='file "+_ext+"' onclick='openMe($(this));'  data-path='"+Main.session.project_folder + Utils.path.sep + folder_content[i]+"' href='#'><span class='glyphicon glyphicon-file'></span> &nbsp;"+folder_content[i] +"</a></li>");
+		//var folder_content = Utils.readDir(Main.session.project_folder);
+		plugin.misterpah.ProjectTree.projectFolder = project_folder;
+		var folder_content = Utils.readDir(project_folder);
+		
+		$("#tree_position").html("<br/><div id='file_tree_path'></div><ul id='file_tree'></ul>");
+		$("#file_tree_path").html("<a onClick='plugin.misterpah.ProjectTree.show_project_tree(plugin.misterpah.ProjectTree.projectFolder+Utils.path.sep+\"..\")'><span class=\"glyphicon glyphicon-upload\"></span>&nbsp;Go up one folder</a>");
+		
+		//Utils.fs.realpathSync("/home/pah/temporary/abc/..")
+		
+		
+		
+		for (i = 0;i< folder_content.length;i++)
+			{
+			var isfile = Utils.isFile(project_folder + Utils.path.sep + folder_content[i]);
+			if (isfile)
+				{
+				var _temp= folder_content[i];
+				var _ext = _temp.split(".").pop();		
+				$("#file_tree").append("<li><a class='file "+_ext+"'  onclick='plugin.misterpah.ProjectTree.openMe($(this));'  data-path='"+project_folder + Utils.path.sep + folder_content[i]+"' href='#'><span class='glyphicon glyphicon-file'></span> &nbsp;"+folder_content[i] +"</a></li>");
+				}
+			else
+				{
+				$("#file_tree").append("<li><a class='folder'  onClick='plugin.misterpah.ProjectTree.openFolder($(this))'  data-path='"+project_folder + Utils.path.sep + folder_content[i]+"'>+ <span class='glyphicon glyphicon-folder-open'></span> &nbsp;"+folder_content[i] +"</a><ul data-path='"+project_folder + Utils.path.sep + folder_content[i]+"'></ul></li>");	
+				}
+			}
+			
+
+      			
 		}
-	else
-		{
-		$("#file_tree").append("<li><a class='folder' onClick='openFolder($(this))'  data-path='"+Main.session.project_folder + Utils.path.sep + folder_content[i]+"'>+ <span class='glyphicon glyphicon-folder-open'></span> &nbsp;"+folder_content[i] +"</a><ul data-path='"+Main.session.project_folder + Utils.path.sep + folder_content[i]+"'></ul></li>");	
-		}
-	}
+		
+	plugin.misterpah.ProjectTree.show_project_tree(Main.session.project_folder);
+})();
+
+		$('body').contextmenu({
+			target: '#test',
+			onItem: function (context, e) {
+			  alert($(e.target).text());
+			}
+		});
