f017d7f3e9df962426ed2582e75deab96ebf247c
misterpah <misterpah@gmail.com>
2014-06-25 20:14:48 +0800

    bump version to 0.3.5

--- source/Main.hx
+++ source/Main.hx
@@ -18,7 +18,7 @@ import jQuery.*;
 
 	static public function run_haxe_studio():Void
 		{
-		version = "0.3.4";
+		version = "0.3.5";
 		session = new Session();
 		file_stack = new FileObject();
 		message = new Message();
