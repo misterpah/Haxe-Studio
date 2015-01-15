1d7dc4f2c6813d3be57de05aa44760fc61758ee8
misterpah <misterpah@gmail.com>
2015-01-16 00:18:52 +0800

    add about page. seperate version file from index.html

---
+++ core/js/hs.version.js
@@ -0,0 +1,9 @@
+hs.version = 0.5;
+hs.releaseNote = {};
+
+
+hs.releaseNote['0.5'] = [];
+hs.releaseNote['0.5'].push("Complete rewrite in Javascript");
+hs.releaseNote['0.5'].push("increase speed by converting watch object to promises");
+hs.releaseNote['0.5'].push("fix inconsistence in multi os by change menu to HTML");
+
