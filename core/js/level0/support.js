059135e5d88f0396f372a32e21e7407b47ae6e0f
misterpah <misterpah@gmail.com>
2015-02-03 00:15:48 +0800

    add function to fetch parameter in js function

--- core/js/level0/support.js
+++ core/js/level0/support.js
@@ -337,7 +337,16 @@ support.exec = function (lines_to_exec,callbackFunction)
 
 
 }
-	
-	
-	
+
+
+function getParameterInFunction(func) {
+  var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+  var ARGUMENT_NAMES = /([^\s,]+)/g;
+  var fnStr = func.toString().replace(STRIP_COMMENTS, '');
+  var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+  if(result === null)
+     result = [];
+  return result;
+}
+
 
