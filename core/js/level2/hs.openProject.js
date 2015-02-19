7285d3e478f04ba4db9b903553d62cf859b9bb3e
misterpah <misterpah@gmail.com>
2015-02-19 21:29:52 +0800

    add ability to open project directly using filename

--- core/js/level2/hs.openProject.js
+++ core/js/level2/hs.openProject.js
@@ -17,4 +17,23 @@
 					}
 				})
 		});
+		
+	central.event.listen("FileMenu.openProjectDirect",function(p1)
+		{
+		Q.fcall(function()
+				{
+				return project.open_project(p1['message']);
+				})
+			.then(function(data)
+				{
+				if (data != "fail")
+					{
+					central.project.projectOpened = true;
+					project.create_project_tree(central.project.projectParameter);
+					central.event.broadcast("openProject.complete","hs.openProject.js","");
+					project.compile_project_ui();
+					}
+				})
+		});		
+		
 })(hs);
