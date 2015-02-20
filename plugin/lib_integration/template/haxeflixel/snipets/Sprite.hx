ba7005e2b2222dd22cc75090cc05d965b1fb602d
misterpah <misterpah@gmail.com>
2015-02-20 15:42:43 +0800

    add Haxeflixel Sprite snipets

---
+++ plugin/lib_integration/template/haxeflixel/snipets/Sprite.hx
@@ -0,0 +1,26 @@
+package %HAXEFLIXEL:PACKAGE%;
+
+import flixel.FlxSprite;
+
+class %HAXEFLIXEL:CLASS% extends FlxSprite
+{
+	public function new()
+	{
+		super();
+	}
+	
+	override public function update():Void 
+	{
+		super.update();
+	}
+	
+	override public function kill():Void
+	{
+		super.kill();
+	}
+	
+	override public function destroy():Void
+	{
+		super.destroy();
+	}	
+}
