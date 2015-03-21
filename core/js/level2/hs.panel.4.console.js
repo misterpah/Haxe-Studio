0c6546b51e7f42d60744b7b829a3ac78c57a7813
misterpah <misterpah@gmail.com>
2015-03-21 23:31:53 +0800

    add listener to show/hide console

--- core/js/level2/hs.panel.4.console.js
+++ core/js/level2/hs.panel.4.console.js
@@ -1,7 +1,28 @@
 (function(){	
 	// ctrl/meta + `
+	
+	
+central.event.listen("ViewMenu.showHideConsole",function(){
+		if ($("#floating_position").css("display") == "block")
+			{
+			$("#floating_position").css("display","none");
+			$("#content_position").css("height","100%");
+			central.event.broadcast("window_resized","index","");
+			debug.debug("console","hide");
+			}
+		else
+			{
+			$("#floating_position").css("display","block");
+			$("#content_position").css("height","70%");
+			central.event.broadcast("window_resized","index","");
+			debug.debug("console","show");
+			}
+
+});	
+	
+	
 	$.keyStroke( 192, { modKeys: [config.ctrlOrMeta] }, function(){   
-	console.log($("#floating_position").css("display"));
+	//console.log($("#floating_position").css("display"));
 	
 		if ($("#floating_position").css("display") == "block")
 			{
