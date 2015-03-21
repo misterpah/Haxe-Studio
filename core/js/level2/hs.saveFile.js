712c4ffe4187417bdc783fd4f2a9f686894ca64d
misterpah <misterpah@gmail.com>
2015-03-21 23:33:15 +0800

    new blank file feature backend

--- core/js/level2/hs.saveFile.js
+++ core/js/level2/hs.saveFile.js
@@ -13,9 +13,46 @@
 			console.log("no active file!");
 			return;
 			}	
-		console.log(filename);
 		var _cur = editor.getCursor();
 		central.filesystem.fileStack[encodeURIComponent(central.filesystem.fileActive)].content = editor._cm.doc.getValue(); //central.filesystem.fileStack[encodeURIComponent(central.filesystem.fileActive)].doc.getValue();
+		
+		
+		if (central.filesystem.fileActive.indexOf("newFile") == 0)
+			{
+			filesystem.file_dialogSaveAs()
+				.then(function(filename)
+					{
+					debug.error(filename);
+					//central.event.broadcast("filesystem.closeFile","hs.saveFile.js",filename);
+					if (support.node.fs.createFileSync(filename))
+						{
+						support.node.fs.writeFileSync(filename,central.filesystem.fileStack[encodeURIComponent(central.filesystem.fileActive)].content);
+						
+						editor.remove_tab(encodeURIComponent(central.filesystem.fileActive));
+						delete central.filesystem.fileStack[encodeURIComponent(central.filesystem.fileActive)]
+						//filesystem.closeFile(encodeURIComponent(filename));
+						
+						
+						
+						central.event.broadcast("FileMenu.openFileDirectly","hs.saveFile.js",filename);
+						}
+					else
+						{
+
+						debug.info("File already exists! replacing...");
+						support.node.fs.writeFileSync(filename,central.filesystem.fileStack[encodeURIComponent(central.filesystem.fileActive)].content);
+
+
+						editor.remove_tab(encodeURIComponent(central.filesystem.fileActive));
+						delete central.filesystem.fileStack[encodeURIComponent(central.filesystem.fileActive)]
+
+						//central.event.broadcast("filesystem.closeFile","hs.saveFile.js",filename);
+						central.event.broadcast("FileMenu.openFileDirectly","hs.saveFile.js",filename);						
+						}
+					});
+			//
+			return;
+			}
 		filesystem.saveFile(encodeURIComponent(central.filesystem.fileActive));
 		$("#editor_tab .active .status_icon").removeClass("glyphicon-record");		
 		$("#editor_tab .active .status_icon").addClass("glyphicon-remove-circle");
